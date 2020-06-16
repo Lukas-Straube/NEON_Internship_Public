efd7876f1a3a86bdc02c4ba3474aa169ce8029b5
Lukas Straube <66032329+Lukas-Straube@users.noreply.github.com>
2020-06-15 19:34:00 -0500

    Update YELL_Data_Explorations.js

--- Yellowstone/YELL_Data_Explorations.js
+++ Yellowstone/YELL_Data_Explorations.js
@@ -1,44 +1,37 @@
 var YELL = ee.Image("users/jadler/asset_20200204025808999");
 
-
-Map.setCenter(-110.51996,44.9198,12)
-Map.addLayer(YELL,{min:1,max:1000, bands:["band53","band35","band19"]},"RGB")
-
-
+Map.setCenter(-110.51996,44.9198,12);
+Map.addLayer(YELL,{min:1,max:1000, bands:["band53","band35","band19"]},"RGB");
 
 var falseColor = function()
 {
-  var Bands_5 = YELL.select("band96")
-  var Bands_4 = YELL.select("band56")
-  var Bands_3 = YELL.select("band37")
+  var Bands_5 = YELL.select("band96");
+  var Bands_4 = YELL.select("band56");
+  var Bands_3 = YELL.select("band37");
 
-  var falseColor = Bands_5.addBands(Bands_4)
-  falseColor = falseColor.addBands(Bands_3)
+  var falseColor = Bands_5.addBands(Bands_4);
+  falseColor = falseColor.addBands(Bands_3);
   
-  var vizParams = {min: 1, max: 6000, gamma: [0.95, 1.3, 1.5]}
+  var vizParams = {min: 1, max: 6000, gamma: [0.95, 1.3, 1.5]};
   
-  Map.addLayer(falseColor, vizParams,"False Color")
-}
+  Map.addLayer(falseColor, vizParams,"False Color");
+};
 
-falseColor()
-
-
+falseColor();
 
 var LandsatToNIS = 
 {
   "Landsat8_B1":[10,16],"Landsat8_B2":[16,28],"Landsat8_B3":[30,44],
   "Landsat8_B4":[52,58],"Landsat8_B5":[94,100],"Landsat8_B6":[237,255],
   "Landsat8_B7":[345,382],"Landsat8_B8":[24,61],"Landsat8_B9":[197,201],
-  "Landsat8_B10":-1,"Landsat8_B11":-1
-}
-
+};
 
 var bandRange = function(array)
 {
   //Take the first index in the array and set it to be start
-  var start = array[0]
+  var start = array[0];
   //Take the second index in the array and set it to be start
-  var end = array[1]
+  var end = array[1];
   
   //Create an empty array to be returned
   var bands = [];
@@ -52,32 +45,28 @@ var bandRange = function(array)
   return bands;
 };
 
-
-
 var NDVI = function()
 {
   //var R_Bands = YELL.select(bandRange(52,58))
-  var R_Bands = YELL.select(bandRange(LandsatToNIS["Landsat8_B4"]))
+  var R_Bands = YELL.select(bandRange(LandsatToNIS["Landsat8_B4"]));
 
   //var NIR_Bands = YELL.select(bandRange(94,100))
-  var NIR_Bands = YELL.select(bandRange(LandsatToNIS["Landsat8_B5"]))
+  var NIR_Bands = YELL.select(bandRange(LandsatToNIS["Landsat8_B5"]));
   
   var NIR_Image = NIR_Bands.reduce(ee.Reducer.mean());
-  NIR_Image = NIR_Image.rename(["NIR"])
+  NIR_Image = NIR_Image.rename(["NIR"]);
 
   var R_Image = R_Bands.reduce(ee.Reducer.mean());
-  R_Image = R_Image.rename(["R"])
+  R_Image = R_Image.rename(["R"]);
   
-  var both_NIR_and_R = NIR_Image.addBands(R_Image)
+  var both_NIR_and_R = NIR_Image.addBands(R_Image);
 
-  var NDVI = both_NIR_and_R.normalizedDifference(["NIR","R"])
+  var NDVI = both_NIR_and_R.normalizedDifference(["NIR","R"]);
 
   Map.centerObject(YELL, 12);
-  var ndviViz={min:-0.5, max:1,palette:['blue', 'brown', 'white']}
+  var ndviViz={min:-0.5, max:1,palette:['blue', 'brown', 'white']};
   Map.addLayer(NDVI, ndviViz,"NDVI");
 }
 
-
-
-NDVI()
+NDVI();
 
