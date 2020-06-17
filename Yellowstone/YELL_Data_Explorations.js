d7bd06ca9448a5b7072a1dc3d19878430b070fcb
Lukas Straube <66032329+Lukas-Straube@users.noreply.github.com>
2020-06-16 19:33:16 -0500

    Update YELL_Data_Explorations.js

--- Yellowstone/YELL_Data_Explorations.js
+++ Yellowstone/YELL_Data_Explorations.js
@@ -1,5 +1,3 @@
-var YELL = ee.Image("users/jadler/asset_20200204025808999");
-
 Map.setCenter(-110.51996,44.9198,12);
 Map.addLayer(YELL,{min:1,max:1000, bands:["band53","band35","band19"]},"RGB");
 
@@ -48,7 +46,7 @@ var bandRange = function(array)
 var NDVI = function()
 {
   //var R_Bands = YELL.select(bandRange(52,58))
-  var R_Bands = YELL.select(bandRange(LandsatToNIS["Landsat8_B4"]));
+  var RED_Bands = YELL.select(bandRange(LandsatToNIS["Landsat8_B4"]));
 
   //var NIR_Bands = YELL.select(bandRange(94,100))
   var NIR_Bands = YELL.select(bandRange(LandsatToNIS["Landsat8_B5"]));
@@ -56,17 +54,55 @@ var NDVI = function()
   var NIR_Image = NIR_Bands.reduce(ee.Reducer.mean());
   NIR_Image = NIR_Image.rename(["NIR"]);
 
-  var R_Image = R_Bands.reduce(ee.Reducer.mean());
-  R_Image = R_Image.rename(["R"]);
+  var RED_Image = RED_Bands.reduce(ee.Reducer.mean());
+  RED_Image = RED_Image.rename(["RED"]);
   
-  var both_NIR_and_R = NIR_Image.addBands(R_Image);
+  var both_NIR_and_RED = NIR_Image.addBands(RED_Image);
 
-  var NDVI = both_NIR_and_R.normalizedDifference(["NIR","R"]);
+  var NDVI = both_NIR_and_RED.normalizedDifference(["NIR","RED"]);
 
   Map.centerObject(YELL, 12);
   var ndviViz={min:-0.5, max:1,palette:['blue', 'brown', 'white']};
   Map.addLayer(NDVI, ndviViz,"NDVI");
-}
+};
 
 NDVI();
 
+var EVI = function()
+{
+  var NIR_Bands = YELL.select(bandRange(LandsatToNIS["Landsat8_B5"]))
+  var NIR_Image = NIR_Bands.reduce(ee.Reducer.mean());
+  NIR_Image = NIR_Image.rename(["NIR"]);
+  
+  var RED_Bands = YELL.select(bandRange(LandsatToNIS["Landsat8_B4"]))
+  var RED_Image = RED_Bands.reduce(ee.Reducer.mean());
+  RED_Image = RED_Image.rename(["RED"]);
+  
+  var BLUE_Bands = YELL.select(bandRange(LandsatToNIS["Landsat8_B2"]))
+  var BLUE_Image = BLUE_Bands.reduce(ee.Reducer.mean());
+  BLUE_Image = BLUE_Image.rename(["BLUE"]);
+  
+  var imageCombination = BLUE_Image.addBands(RED_Image);
+  imageCombination = imageCombination.addBands(NIR_Image)
+  
+  print(imageCombination)
+  
+  var EVI_Image = imageCombination.expression
+  (
+      'G * ((NIR - RED) / (NIR + C1 * RED - C2 * BLUE + L))', 
+      {
+        'NIR': imageCombination.select('NIR').multiply(0.0001).float(),
+        'RED': imageCombination.select('RED').multiply(0.0001).float(),
+        'BLUE': imageCombination.select('BLUE').multiply(0.0001).float(),
+        'G':2.5,
+        'C1':6,
+        'C2':7.5,
+        'L':1
+      }
+  );
+  
+  Map.centerObject(YELL, 12);
+  Map.addLayer(EVI_Image, {min: -1, max: 1, palette: ['EBDED4', '0A2003']}, "EVI" );
+};
+
+EVI();
